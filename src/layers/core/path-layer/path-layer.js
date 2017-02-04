793ad3827fbe50ebc4c91f7030dd4d5b38d7976a
DC <dcposch@dcpos.ch>
2017-02-04 01:44:43 -0800

    PathLayer: support color-per-point

--- src/layers/core/path-layer/path-layer.js
+++ src/layers/core/path-layer/path-layer.js
@@ -303,15 +303,32 @@ export default class PathLayer extends Layer {
 
     let i = 0;
     paths.forEach((path, index) => {
-      const pointColor = getColor(data[index], index);
-      if (isNaN(pointColor[3])) {
-        pointColor[3] = 255;
-      }
-      for (let ptIndex = 1; ptIndex < path.length; ptIndex++) {
-        value[i++] = pointColor[0];
-        value[i++] = pointColor[1];
-        value[i++] = pointColor[2];
-        value[i++] = pointColor[3];
+      const color = getColor(data[index], index) || DEFAULT_COLOR;
+      if (Array.isArray(color[0])) {
+        if (color.length !== path.length) {
+          throw new Error('PathLayer getColor() returned a color array, but the number of ' +
+           `colors returned doesn't match the number of points in the path. Index ${index}`);
+        }
+        color.forEach((pointColor) => {
+          const alpha = isNaN(pointColor[3]) ? 255 : pointColor[3];
+          // two copies for outside edge and inside edge each
+          value[i++] = pointColor[0];
+          value[i++] = pointColor[1];
+          value[i++] = pointColor[2];
+          value[i++] = alpha;
+        });
+      } else {
+        const pointColor = color;
+        if (isNaN(pointColor[3])) {
+          pointColor[3] = 255;
+        }
+        for (let ptIndex = 0; ptIndex < path.length; ptIndex++) {
+          // two copies for outside edge and inside edge each
+          value[i++] = pointColor[0];
+          value[i++] = pointColor[1];
+          value[i++] = pointColor[2];
+          value[i++] = pointColor[3];
+        }
       }
     });
   }
